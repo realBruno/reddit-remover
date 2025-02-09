4bfc90d00313c1b747fdeb8cfb69aa52c805f4ff
realBruno <soubruno@protonmail.com>
2025-02-09 17:37:59 -0300

    Update variable name

--- Content.js
+++ Content.js
@@ -2,16 +2,16 @@ const domain = "reddit.com";
 
 function hide_reddit() {
 
-    const leftoverContent = document.querySelectorAll("div.g");
+    const results = document.querySelectorAll("div.g");
 
-    leftoverContent.forEach(leftover => {
-        const link = leftover.querySelector('a');
+    results.forEach(result => {
+        const link = result.querySelector('a');
         if (!link) return;
 
         const shouldHide = link.href.toLowerCase().includes(domain);
 
         if (shouldHide)
-            leftover.style.display = 'none';
+            result.style.display = 'none';
     });
 }
 
